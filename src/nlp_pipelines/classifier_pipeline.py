9286fbffe9a8ca760836fddf98c630f4729cd76f
lucabella98lakestar <lucabella@ip-10-75-13-169.eu-west-2.compute.internal>
2025-01-16 11:31:12 +0100

    changed structure ples topic descritor code

--- src/nlp_pipelines/classifier_pipeline.py
+++ src/nlp_pipelines/classifier_pipeline.py
@@ -1,23 +1,11 @@
-import re
-import nltk
-#from nltk.corpus import satopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from openai import OpenAI
-from math import sqrt  # Importing sqrt from the math module
-from bertopic import BERTopic
-import random
-from supabase import create_client
-import time
-import tiktoken
-from sqlalchemy import create_engine, MetaData, Table, select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy                                                 import select
 
-from bertopic_classifier import BERTopicClassifier
-from embedder import EmbeddingPipeline
+from ddvc.src.nlp_pipelines.utils.bertopic_classifier           import BERTopicClassifier
+from ddvc.src.nlp_pipelines.utils.embedder                      import EmbeddingPipeline
+from ddvc.src.nlp_pipelines.utils.topic_descriptor              import TopicDescriptor
 
 # Import the database session and schema from db.py
-from utils.db import session, schema
+from utils.db                                                   import session, schema
 
 def classify_companies():
     """
@@ -43,6 +31,9 @@ def classify_companies():
 
     bertopic_classifier = BERTopicClassifier()
     companies_with_topics = bertopic_classifier.classify_bertopic(companies_with_embeddings)
+
+    topic_descriptor = TopicDescriptor()
+    topic_descriptor.get_topic_descriptions_and_upsert_in_db()
 
     return companies_with_topics
 
