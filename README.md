52520f3bf36a22a9b5281c0ded4bb95ec9b9e119
Alex Patow <ap@inflection.xyz>
2025-01-16 12:30:53 +0100

    cleanup + readme

--- README.md
+++ README.md
@@ -3,3 +3,47 @@
 ![alt text](https://i.imgur.com/O8vZHPM.png)
 
 > This project was built as part of the Data-Driven VC Hackathon organized by [Red River West](https://redriverwest.com) & [Bivwak! by BNP Paribas](https://bivwak.bnpparibas/)
+
+## Prerequisites
+
+### Rye
+
+```bash
+brew install rye
+```
+
+```bash
+rye sync
+```
+
+### Supabase
+
+We're using [Supabase](https://supabase.com) as our database, you'll need to create a project and get the database URL.
+
+## Table Schema
+
+See [docs/schema.md](docs/schema.md)
+
+## Code Structure
+
+- [src/collect](src/collect): Data collection scripts
+- [src/nlp_pipeline](src/nlp_pipeline): NLP pipeline scripts
+- [src/utils](src/utils): Utility scripts (e.g. database connection)
+- [front](front): Frontend code
+
+## Data collection
+
+Add a `.env` file in the root of the project based on `.env.example`
+
+Run the following commands to collect data:
+
+```bash
+rye run harmonic
+rye run predictleads
+rye run predictleads_news
+rye run pdl_headcount_sales_eng
+```
+
+## NLP pipeline
+
+See [docs/nlp_pipeline.md](docs/nlp_pipeline.md)
--- src/collect/harmonic.py
+++ src/collect/harmonic.py
@@ -1,141 +1,139 @@
 import os
-
 import typing
-import requests
 
+import pandas as pd
+import requests
 import sqlalchemy
 
-import numpy as np
-import pandas as pd
-
 import src.utils as utils
+
 db = utils.db
 
-BASE_URL = 'https://api.harmonic.ai'
+BASE_URL = "https://api.harmonic.ai"
+
 
 def get_companies(cursor: typing.Optional[str] = None):
-  headers = {
-    'apikey': os.environ.get('HARMONIC_API_KEY')
-  }
+    headers = {"apikey": os.environ.get("HARMONIC_API_KEY")}
 
-  URL = f'{BASE_URL}/savedSearches:results/129627?size=100'
+    URL = f"{BASE_URL}/savedSearches:results/129627?size=100"
 
-  if cursor:
-    URL = f'{URL}&cursor={cursor}'
+    if cursor:
+        URL = f"{URL}&cursor={cursor}"
 
-  response = requests.get(URL, headers=headers)
+    response = requests.get(URL, headers=headers)
 
-  if response.status_code >= 400:
-    print('Error:', response.status_code)
-    return
-  
-  data = response.json()
-  return data
+    if response.status_code >= 400:
+        print("Error:", response.status_code)
+        return
+
+    data = response.json()
+    return data
+
 
 done: bool = False
-cursor: str = ''
+cursor: str = ""
 page = 0
 
 while not done:
     page += 1
 
-    print(f'Page: {page}')
+    print(f"Page: {page}")
     data = get_companies(cursor)
-    
+
     if not data:
         continue
-    
-    cursor = data.get('page_info', {}).get('next', '')
-    has_next = data.get('page_info', {}).get('has_next', False)
+
+    cursor = data.get("page_info", {}).get("next", "")
+    has_next = data.get("page_info", {}).get("has_next", False)
 
     if not has_next:
         done = True
 
-    results = data.get('results', [])
+    results = data.get("results", [])
     companies = []
 
     for result in results:
         founded_at = None
 
         try:
-            founded_at = result.get('founding_date', {}).get('date')
+            founded_at = result.get("founding_date", {}).get("date")
         except:
             pass
-        
+
         location = None
 
         try:
-            location = result.get('location', {}).get('country')
+            location = result.get("location", {}).get("country")
         except:
             pass
-        
+
         domain = None
 
         try:
-           domain = result.get('website', {}).get('domain')
+            domain = result.get("website", {}).get("domain")
         except:
             pass
-        
+
         funding_stage = None
         funding_total = None
 
         try:
-            funding_stage = result.get('funding', {}).get('funding_stage')
-            funding_total = result.get('funding', {}).get('funding_total')
+            funding_stage = result.get("funding", {}).get("funding_stage")
+            funding_total = result.get("funding", {}).get("funding_total")
         except:
             pass
-        
-        tags = [
-           tag.get('display_value') for tag in result.get('tags', [])
-        ]
-        tags_v2 = [
-           tag.get('display_value') for tag in result.get('tags_v2', [])
-        ]
+
+        tags = [tag.get("display_value") for tag in result.get("tags", [])]
+        tags_v2 = [tag.get("display_value") for tag in result.get("tags_v2", [])]
 
         tags = list(set(tags + tags_v2))
 
         company = {
-           'id': result.get('id'),
-           'name': result.get('name'),
-           'domain': domain,
-           'logo_url': result.get('logo_url'),
-           'description': result.get('description'),
-           'external_description': None,
-           'country': location,
-           'type': result.get('company_type'),
-           'headcount': result.get('headcount'),
-           'funding_stage': funding_stage,
-           'funding_total': funding_total,
-           'founded_at': founded_at,
-           'tags': tags,
+            "id": result.get("id"),
+            "name": result.get("name"),
+            "domain": domain,
+            "logo_url": result.get("logo_url"),
+            "description": result.get("description"),
+            "external_description": None,
+            "country": location,
+            "type": result.get("company_type"),
+            "headcount": result.get("headcount"),
+            "funding_stage": funding_stage,
+            "funding_total": funding_total,
+            "founded_at": founded_at,
+            "tags": tags,
         }
 
         companies.append(company)
 
         data = []
 
-        for key in result.get('traction_metrics', {}).keys():
-            timeseries = result.get('traction_metrics', {}).get(key, {}).get('metrics', [])
+        for key in result.get("traction_metrics", {}).keys():
+            timeseries = (
+                result.get("traction_metrics", {}).get(key, {}).get("metrics", [])
+            )
 
             for item in timeseries:
-                data.append({
-                    'company_id': result.get('id'),
-                    'type': key,
-                    'date': item.get('timestamp'),
-                    'value': item.get('metric_value'),
-                    'source': 'harmonic'
-                }) 
-            
+                data.append(
+                    {
+                        "company_id": result.get("id"),
+                        "type": key,
+                        "date": item.get("timestamp"),
+                        "value": item.get("metric_value"),
+                        "source": "harmonic",
+                    }
+                )
 
     if len(companies) > 0:
-        print(f'Upserting {len(companies)} companies')
-        upsert_batch = sqlalchemy.dialects.postgresql.insert(db.schema.classes.companies).values(companies)
+        print(f"Upserting {len(companies)} companies")
+        upsert_batch = sqlalchemy.dialects.postgresql.insert(
+            db.schema.classes.companies
+        ).values(companies)
         upsert_batch = upsert_batch.on_conflict_do_update(
-            index_elements=['id'],
+            index_elements=["id"],
             set_={
-                key: getattr(upsert_batch.excluded, key)
-                for key in companies[0].keys()
-            }
+                key: getattr(upsert_batch.excluded, key) for key in companies[0].keys()
+            },
         )
 
         try:
@@ -146,29 +144,31 @@ while not done:
             print(e)
             db.session.rollback()
             break
-        
+
     if len(data) > 0:
-        print(f'Upserting {len(data)} data points')
+        print(f"Upserting {len(data)} data points")
         data = pd.DataFrame(data)
-        data = data.drop_duplicates(subset=['company_id', 'type', 'date', 'source'], keep='first')
+        data = data.drop_duplicates(
+            subset=["company_id", "type", "date", "source"], keep="first"
+        )
         data = data.dropna()
-        data = data.to_dict(orient='records')
-        
+        data = data.to_dict(orient="records")
 
         BATCH_SIZE = 5_000
         batch_count = len(data) // BATCH_SIZE + 1
 
         for i in range(batch_count):
-            print(f'Batch {i + 1}/{batch_count}')
-            batch = data[i * BATCH_SIZE: (i + 1) * BATCH_SIZE]
+            print(f"Batch {i + 1}/{batch_count}")
+            batch = data[i * BATCH_SIZE : (i + 1) * BATCH_SIZE]
 
-            upsert_batch = sqlalchemy.dialects.postgresql.insert(db.schema.classes.harmonic_data).values(batch)
+            upsert_batch = sqlalchemy.dialects.postgresql.insert(
+                db.schema.classes.harmonic_data
+            ).values(batch)
             upsert_batch = upsert_batch.on_conflict_do_update(
-                index_elements=['company_id', 'type', 'date', 'source'],
+                index_elements=["company_id", "type", "date", "source"],
                 set_={
-                    key: getattr(upsert_batch.excluded, key)
-                    for key in batch[0].keys()
-                }
+                    key: getattr(upsert_batch.excluded, key) for key in batch[0].keys()
+                },
             )
 
             try:
@@ -178,7 +178,5 @@ while not done:
                 print(e)
                 db.session.rollback()
                 break
-        
+
         data = []
-      
-    
