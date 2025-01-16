466d620d86c6dfa0b5d5ba4415851acc8784376b
ludofigueiredo <ludofigueiredo@gmail.com>
2025-01-16 12:29:10 +0100

    Adding the code to retrieve data from ther similarweb api

--- src/utils/db.py
+++ src/utils/db.py
@@ -8,20 +8,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get database URL from environment variables
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Ensure the database URL is set
 if not DATABASE_URL:
-    raise ValueError(
-        'Could not find DATABASE_URL in environment variables')
+    raise ValueError("Could not find DATABASE_URL in environment variables")
 
 # Connect to the database
-connection = sqlalchemy.create_engine(
-    DATABASE_URL, echo=False)
+connection = sqlalchemy.create_engine(DATABASE_URL, echo=False)
 
 # Reflect the database schema
 schema = automap_base()
 schema.prepare(autoload_with=connection, engine=connection)
 
 session = sqlalchemy.orm.Session(connection)
-
